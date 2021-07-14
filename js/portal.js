76f28b40929d71bbd369c88d3dd041ba94cb3a2b
BloomAutist47 <BloomAutist47@users.noreply.github.com>
2021-07-14 23:03:50 +0800

    new

---
+++ js/portal.js
@@ -0,0 +1,31 @@
+
+
+function openInNewTab(link) {
+  window.open(
+    link,
+    '_blank' // <- This is what makes it open in a new window.
+  );
+}
+
+function myFunction() {
+  // For searching items in the downloads modal page
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
