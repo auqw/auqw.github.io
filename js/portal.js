a9d143edc111eebaba715b629b6e621a0596385c
BloomAutist47 <BloomAutist47@users.noreply.github.com>
2021-07-15 23:03:34 +0800

    revision 2

--- js/portal.js
+++ js/portal.js
@@ -29,3 +29,18 @@ function myFunction() {
     }
   }
 }
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  // document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("closeSideBar").style.display = "block";
+  // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  // document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("closeSideBar").style.display = "none";
+  // document.body.style.backgroundColor = "white";
+}
