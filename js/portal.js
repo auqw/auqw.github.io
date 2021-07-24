693d19941c66951635988a442a9b7b3cbe6d168a
BloomAutist47 <BloomAutist47@users.noreply.github.com>
2021-07-24 16:57:59 +0800

    updated grim download modal

--- js/portal.js
+++ js/portal.js
@@ -44,3 +44,17 @@ function closeNav() {
   document.getElementById("closeSideBar").style.display = "none";
   // document.body.style.backgroundColor = "white";
 }
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
