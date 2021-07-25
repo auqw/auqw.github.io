294683c6209663b7b4c17554da5ad8ec7046af11
BloomAutist47 <BloomAutist47@users.noreply.github.com>
2021-07-25 21:09:47 +0800

    fixed dw linkg

--- files/JS Scripts/DNScript.js
+++ files/JS Scripts/DNScript.js
@@ -62,7 +62,7 @@ function takeshot() {
             document.getElementById('output').appendChild(canvas);
             canvas.crossorigin="anonymous"
 			var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
-			console.log(image)
+
 			window.location.href=image; // it will save locally
 
 			  // var link = document.getElementById('screenSave');
