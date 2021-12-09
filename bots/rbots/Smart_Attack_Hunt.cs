c4b1bf63bfc6d5b5c23a2e4bdbd0acfb2518d0e9
BloomAutist47 <bloomautist47@gmail.com>
2021-12-09 19:54:00 +0800

    Added Smart_Attack_Hunt.cs by Exelot

--- bots/rbots/Smart_Attack_Hunt.cs
+++ bots/rbots/Smart_Attack_Hunt.cs
@@ -1,19 +1,25 @@
 using System;
 using RBot;
+using System.Linq;
 
-public class ExeBot {
+public class SmartAttackHunt {
 	
-	/// <summary>
 	/// This bot has two modes: 
 	/// - If you target a monster in-game before activating the bot, it will hunt that monster all across the map (private room is advised).
 	/// - If you dont target a monster in-game before hand, it will attack any monster on screen and will stay on said screen.
 	/// Note that the bot will automatically turn in any quests that are completed while the bot is active.
-	/// </summary>
-	
 	
 	//-----------EDIT BELOW-------------//
 	public int TurnInAttempts = 10;
 	public readonly int[] SkillOrder = { 2, 4, 3, 1 };
+	public string[] GrabTheseDrops = {
+		"Item Name Here",
+		"Item Name Here",
+		"Item Name Here",
+		"Item Name Here",
+		"Item Name Here"
+		//If you need more items than 5, just add more items to this list
+	 };
 	//-----------EDIT ABOVE-------------//
 
 	public ScriptInterface bot => ScriptInterface.Instance;
@@ -26,27 +32,33 @@ public class ExeBot {
 		bot.Options.RestPackets = true;
 		bot.Options.InfiniteRange = false;
 		
+		//Remove the "//" on the line below to activate the automation of picking up drops.
+		GetDropList(GrabTheseDrops);
 		SkillList(SkillOrder);
 		
-		Target = bot.Player.Target.Name;
-		if (Target != null) bot.Log($"[{DateTime.Now:HH:mm:ss}] Hunting \t \t [{Target}]");
+		// Checking if player should hunt or attack
+		if (bot.Player.HasTarget) {
+			Target = bot.Player.Target.Name;
+			bot.Log($"[{DateTime.Now:HH:mm:ss}] Hunting \t \t [{Target}]");
+		}
 		else bot.Log($"[{DateTime.Now:HH:mm:ss}] Attacking \t [Everything]");
-		while(!bot.ShouldExit())
-		{
-			if (Target != null) bot.Player.Hunt(Target);
-			else
-			{
+
+		
+		while(!bot.ShouldExit()) {
+			// Doing the actual Hunting
+			if (bot.Player.HasTarget) bot.Player.Hunt(Target);
+			
+			// The alternative of attacking
+			else while(!bot.ShouldExit()) {
 				bot.Player.SetSpawnPoint();
 				bot.Player.Attack("*");
 			}
 			
-			if (bot.Quests.ActiveQuests.Count >= 1);
-			{
-				foreach (var Quest in bot.Quests.ActiveQuests)
-				{
+			// Auto Quest Turnin
+			if (bot.Quests.ActiveQuests.Count >= 1)	{
+				foreach (var Quest in bot.Quests.ActiveQuests) {
 					QuestID = Quest.ID;
-					if (bot.Quests.CanComplete(QuestID))
-					{
+					if (bot.Quests.CanComplete(QuestID)) {
 						Cell = bot.Player.Cell;
 						Pad = bot.Player.Pad;
 						SafeQuestComplete(QuestID);
@@ -57,13 +69,19 @@ public class ExeBot {
 		}
 	}
 	
+	/*------------------------------------------------------------------------------------------------------------
+													 Required Functions
+	------------------------------------------------------------------------------------------------------------*/
+	//These functions are required for this bot to function.
+
 	/// <summary>
 	/// Spams Skills when in combat. You can get in combat by going to a cell with monsters in it with bot.Options.AggroMonsters enabled or using an attack command against one.
 	/// </summary>
 	public void SkillList(params int[] Skillset)
 	{
-		bot.RegisterHandler(1, b =>
-		{
+		if(bot.Handlers.Any(h => h.Name == "Skill Handler"))
+			bot.Handlers.RemoveAll(h => h.Name == "Skill Handler");
+		bot.RegisterHandler(1, b => {
 			if (bot.Player.InCombat)
 			{
 				foreach (var Skill in Skillset)
@@ -71,7 +89,23 @@ public class ExeBot {
 					bot.Player.UseSkill(Skill);
 				}
 			}
-		});
+		}, "Skill Handler");
+	}
+	
+	/// <summary>
+	/// Checks if items in an array have dropped every second and picks them up if so. GetDropList is recommended.
+	/// </summary>
+	public void GetDropList(params string[] GetDropList)
+	{
+		if(bot.Handlers.Any(h => h.Name == "Drop Handler"))
+			bot.Handlers.RemoveAll(h => h.Name == "Drop Handler");
+		bot.RegisterHandler(4, b => {
+			foreach (string Item in GetDropList)
+			{
+				if (bot.Player.DropExists(Item)) bot.Player.Pickup(Item);
+			}
+			bot.Player.RejectExcept(GetDropList);
+		}, "Drop Handler");
 	}
 	
 	/// <summary>
