8d672ab7e819bb383c65e18b510754192780c267
BloomAutist47 <bloomautist47@gmail.com>
2021-12-17 21:20:45 +0800

    Added Bloom_Army_AC_Checker.cs by Bloom

---
+++ bots/rbots/Bloom_Army_AC_Checker.cs
@@ -0,0 +1,61 @@
+// AC Checker and relogger by BLoom
+using System;
+using RBot;
+using System.Collections.Generic;
+
+
+// OPEN THE LOGS IF YOU WANT THIS TO WORK
+// OPEN THE LOGS IF YOU WANT THIS TO WORK
+// OPEN THE LOGS IF YOU WANT THIS TO WORK
+// OPEN THE LOGS IF YOU WANT THIS TO WORK
+// OPEN THE LOGS IF YOU WANT THIS TO WORK
+
+// THEN CHECK THE SCRIPT TAB OF THE LOGS FOR THE RESULT
+
+public class Script {
+	public ScriptInterface bot => ScriptInterface.Instance;
+
+	public string server = "twig";
+
+	// Place accounts here
+	// {"Username", "Password"}
+	public Dictionary<string, string> accounts = new Dictionary<string, string>(){
+		{"Artix", "daddyArtixPleaseCumInsideMe"},
+		{"Alina", "Give me than Thin ASS!"}
+
+        
+		
+	};
+
+	public void ScriptMain(ScriptInterface bot){
+		if (bot.Player.LoggedIn) {
+            bot.Player.Logout();
+			bot.Sleep(2000);
+		}
+        
+		foreach(var acc in accounts) {
+			while (!bot.Player.LoggedIn) {
+                bot.CallGameFunction("login", acc.Key, acc.Value);
+                bot.Player.Reconnect(server);
+                while (!bot.Player.Loaded) { }
+                while (!bot.Map.Loaded) { }
+
+
+                string coin = bot.GetGameObject<string>("world.myAvatar.objData.intCoins");
+                bot.Log($"[{acc.Key}] {coin} ACs");
+				if (bot.Player.LoggedIn) {
+                    bot.Player.Logout();
+                    bot.Sleep(1500);
+					break;
+				}
+			}
+
+        }
+		bot.Log("\n\nDone");
+	}
+
+
+
+}
+
+
