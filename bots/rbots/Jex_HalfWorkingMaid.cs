bba8bc4d1cba2cc7fcf35a08e96234e53681129b
BloomAutist47 <bloomautist47@gmail.com>
2022-01-28 22:31:39 +0000

    Added Jex_HalfWorkingMaid.cs by Jex

---
+++ bots/rbots/Jex_HalfWorkingMaid.cs
@@ -0,0 +1,168 @@
+﻿using RBot;
+using RBot.Options;
+
+using System;
+using System.Collections.Generic;
+
+public class FollowerScript 
+{
+    public ScriptInterface bot => ScriptInterface.Instance;
+    public string OptionsStorage = "Jex_follower";
+    public List<IOption> Options = new List<IOption>(){
+        new Option<string>("sOrder","Order of Skills","Insert which skills the follower should be using.\nExample : \"3,1,2,4\" (do not add the \"\") " , "3,1,4,2"),
+        new Option<string>("playerName","Player Name","Insert the name of the player to follow","artix"),
+        new Option<bool>("drops","Reject Drops?","Change to \"True\" if you want delete drops and \"False\" to remove them",false)
+    };
+    
+    public int Counter = 0;
+    public void ScriptMain(ScriptInterface bot)
+    {
+        while (!bot.Player.Loaded) { };
+        while (!bot.Map.Loaded) { };
+        SafeMapJoin("yulgar","Wait","Spawn");
+        bot.Sleep(500);
+        SetUp(bot.Config.Get<string>("sOrder"), bot.Config.Get<string>("playerName"));
+        while(bot.Player.LoggedIn)
+        {
+            if(bot.Monsters.Exists("*")){bot.Player.Attack("*");}
+            else {FollowPlayer(bot.Config.Get<string>("playerName"));}
+        }
+    }
+
+
+
+    public void SetUp(string skillOrder,string playerName){
+        bot.SetGameObject("stage.frameRate", 10);
+        if (!bot.GetGameObject<bool>("ui.monsterIcon.redX.visible")){bot.CallGameFunction("world.toggleMonsters");};
+        bot.SetGameObject("litePreference.data.bUntargetSelf",true);
+        bot.SetGameObject("litePreference.data.bSmoothBG",false);
+        bot.SetGameObject("litePreference.data.bUntargetDead",true);
+        bot.SetGameObject("litePreference.data.bCustomDrops",false);
+        bot.Options.CustomName = "Bot made by Jex";
+        bot.Options.CustomGuild = "https://auqw.tk/";
+        bot.Options.AutoRelogin = true;
+        bot.Options.InfiniteRange = true;
+        bot.Options.SkipCutscenes = true;
+        
+        int[] sOrder = Array.ConvertAll(skillOrder.Split(','), s => int.Parse(s));
+        Handlers(sOrder,playerName);
+    }
+
+    public void Handlers(int[] sOrder,string p_Name){
+
+        // Attack monsters
+        bot.RegisterHandler(1,b=>{
+            if(bot.Player.InCombat){
+                foreach(int skill in sOrder){
+                    bot.Player.UseSkill(skill);
+                
+                }
+
+            };
+        });
+
+        // Death Handler
+        bot.RegisterHandler(2, b => {
+            if (bot.Player.State==0) {
+            bot.Player.SetSpawnPoint();
+            ExitCombat();
+            bot.Sleep(12000);
+            }
+        });
+
+        // Follow the player
+        bot.Events.MonsterKilled += b => {
+            if(!bot.Map.PlayerExists(p_Name)){
+                ExitCombat();
+                FollowPlayer(p_Name);
+            }else{
+                bot.Player.Goto(p_Name);
+                Counter = 0;
+            }
+
+        };
+
+        if (bot.Config.Get<bool>("drops")){
+            bot.RegisterHandler(2,b => {
+                if (bot.Player.DropExists("*")){bot.Player.RejectAll();}
+            });
+        }
+
+    }
+
+
+    // ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇
+    //                                                      NORMAL FUNCTIONS (just ignore)
+    // ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇
+
+	public void ExitCombat()
+	{
+		bot.Player.Jump("Wait", "Spawn");
+		while (bot.Player.State == 2) { }
+	}
+
+
+	public void SafeMapJoin(string MapName, string CellName = "Enter", string PadName = "Spawn",string roomNumber ="999999")
+	{
+		string mapname = MapName.ToLower();
+		while (bot.Map.Name != mapname)
+		{
+			ExitCombat();
+			if (mapname == "tercessuinotlim") bot.Player.Jump("m22", "Center");
+			bot.Player.Join($"{mapname}-{roomNumber}", CellName, PadName);
+			bot.Wait.ForMapLoad(mapname);
+			bot.Sleep(500);
+		}
+		if (bot.Player.Cell != CellName) bot.Player.Jump(CellName, PadName);
+	}
+
+    public void FollowPlayer(string playerName){
+        Stuff:
+            bot.SendPacket($"%xt%zm%cmd%1%goto%{playerName}%");
+            while (!bot.Map.Loaded) { };
+            while (!bot.Player.Loaded) { };
+            bot.Sleep(500); 
+            if(bot.Map.GetPlayer(playerName) == null) {
+                bot.Sleep(6000);
+                if(Counter == 6) {bot.Player.Logout();ScriptManager.StopScript();}
+                Counter++;
+                goto Stuff;
+            }else {bot.Player.Goto(playerName);}
+    }
+
+
+
+    /*  
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⡀⠄⠄⠄⠄⠄⠄⢠⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⣷⡀⠄⠄⠄⠄⠄⢸⡆⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢻⣷⡀⠄⠄⠄⠄⠘⣿⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⢿⣿⣅⠂⢀⠄⠄⠹⣿⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣀⣠⣤⣤⣤⣤⣀⡈⠻⣿⣦⡀⠳⣄⠄⢻⣿⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠄⠄⠄⠄⠈⠉⠻⢿⣷⣿⣿⣿⣦⡘⢷⣮⣿⣿⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠉⠙⠳⣶⣶⣤⣤⣹⣿⣿⣿⣿⣿⣦⣿⣿⣿⣿⣤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⠤⢀⡠⠶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠁⣨⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠋⠁⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣧⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠿⣿⣿⣿⣷⡁⠨⠿⣿⣿⣷⡆⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠈⠻⣿⣿⣿⣄⡚⠡⢻⠟⠃⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠄⠄⠄⠸⢿⣿⣿⠇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⠄⠄⠄⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢿⡿⣿⣿⣿⣿⣿⣿⣿⠟⠘⡟⣿⣿⣿⣿⣿⣿⣗⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⡇⠹⣿⣿⣿⠟⠉⠄⠄⢰⠃⢸⣿⣿⣿⣿⣿⣿⣿⣶⣄⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢻⡿⠁⠄⠄⠄⠄⠊⠄⠘⠏⠉⠋⠋⠋⠉⠋⠋⠋⠓⠒⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+    */
+}
